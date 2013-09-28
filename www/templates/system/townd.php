024e7b08811d44fb773dc338a9f11356b5ff26be
Eighke <eighke@multi-site.net>
2013-09-29 03:28:18 +0700

    Updated `townd` template to new structure

--- www/templates/system/townd.php
+++ www/templates/system/townd.php
@@ -4,67 +4,33 @@
  * Subpackage	System
  * File			townd.php
  *
- * Licence		GNU General Public License version 3; see http://www.gnu.org/licenses/lgpl-3.0.en.html
- * Copyright	Copyright (C) 2005 - 2012 Frédéric Vandebeuque. All rights reserved.
- * Contrib		Frédéric Vandebeuque (fred.vdb@newebtime.com)
+ * Licence		Mozilla Public License, v. 2.0; see http://mozilla.org/MPL/2.0/
+ * Copyright	Copyright (C) 2005 - 2013 Frédéric Vandebeuque. All rights reserved.
+ * Contrib		Frédéric V. (fred.vdb@newebtime.com)
  * 				Eighke (eighke@multi-site.net)
  *
- * Version		2012-11-17 - Eighke
+ * Version		2013-02-18 - Eighke
  */
 if (!session_id()) exit();
 ?>
-<!--  Wrapper -->
-<div id="wrapper">
-	<?php $this->renderMenu(); ?>
-	<!-- Main Block -->
-	<div id="main">
-		<!-- Top -->
-		<div class="top">
-			<div class="left"></div>
-			<div class="center"></div>
-			<div class="right"></div>
-		</div>
-		<!-- /Top -->
-		<!-- Middle -->
-		<div class="middle">
-			<div class="outer">
-				<div class="inner">
-					<!-- #Main -->
-					<div class="main">
-						<h1><?php echo ILang::_('Confirmation'); ?></h1>
-						<?php $this->renderMsgs(); ?>
-						<form action="./townd.php" method="POST">
-							<div class="content">
-								<?php echo ILang::_('AreYouSure?'); ?><br /><?php echo ILang::_('WeNeedPassword'); ?>
-							</div>
-							<div class="fields">
-								<div class="label">
-									<label for="field-crtPwd"><?php echo ILang::_('crtPasword'); ?></label>
-								</div>
-								<div class="field">
-									<input type="password" name="crtPwd" id="field-crtPwd" class="required" />
-								</div>
-							</div>
-							<div id="navigation">
-								<hr />
-								<input class="navigation_button" id="next" value="<?php echo ILang::_('Submit'); ?>" name="delTwn" type="submit" />
-							</div>
-							
-						</form>
-					</div>
-					<!-- /#Main -->
-				</div>
-			</div>
-		</div>
-		<!-- /Middle -->
-		<!-- Bottom -->
-		<div class="bottom">
-			<div class="left"></div>
-			<div class="center"></div>
-			<div class="right"></div>
-		</div>
-		<!-- /Bottom -->
+<h1><?php echo ILang::_('Confirmation'); ?></h1>
+<?php $this->renderMsgs(); ?>
+<form action="./townd.php" method="POST">
+	<div class="content">
+		<?php echo ILang::_('AreYouSure?'); ?><br /><?php echo ILang::_('WeNeedPassword'); ?>
 	</div>
-	<!-- /Main Block -->
-</div>
-<!--  /Wrapper -->
+	<div class="fields">
+		<div class="label">
+			<label for="field-crtPwd"><?php echo ILang::_('crtPasword'); ?></label>
+		</div>
+		<div class="field">
+			<input type="password" name="crtPwd" id="field-crtPwd" class="required" />
+		</div>
+	</div>
+	<div id="navigation">
+		<hr />
+		<input type="hidden" name="id" value="<?php echo $this->getData('id'); ?>" />
+		<input class="navigation_button" id="next" value="<?php echo ILang::_('Submit'); ?>" name="delTwn" type="submit" />
+	</div>
+	
+</form>
